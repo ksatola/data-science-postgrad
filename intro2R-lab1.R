5ccd512543de002497b40ae1b6c335c322fb9883
Krzysztof Satola <krzysztof@satola.net>
2019-10-19 11:59:15 +0200

    Add files via upload

---
+++ intro2R-lab1.R
@@ -0,0 +1,48 @@
+R.version.string
+
+getRversion()
+
+help(help)
+
+?help
+
+?"+"
+
+library(help = "stats")
+
+2+2
+
+2^5
+
+FALSE == FALSE
+
+T == TRUE
+
+3 > 2
+
+2 +
+2  
+
+print("My name is Inigo Montoya. You killed my father. Prepare to die.")
+
+?print
+
+cat("My name is Inigo Montoya. You killed my father. Prepare to die.")
+
+?cat
+
+cat('aa', 'bb')
+
+?c
+
+cat(xs, ys)
+
+print("My name is Inigo Montoya.", "You killed my father.", "Prepare to
+die.") # !!!
+
+ca # naciskamy klawisz [TAB], wybieramy z listy "cat", otwieramy nawias i... kontynuujemy wg nastepnej linii :)
+ cat(c('a','b'))
+cat('a', 'b')
+
+
+
---
+++ stat-1_RScript1.R
@@ -0,0 +1,47 @@
+x <- 2 # jednoelementowy wektor
+y <- x + 5
+
+# Create vector
+xs <- c(1,2,3,4,5)
+ys <- c(10,20,30,40,50)
+
+zs <- 1:100
+
+zs
+
+# Element-wise sum
+z1 <- xs+ys
+
+z2 <- xs+1
+
+z4 <- xs^2
+
+# Concatenation
+z3 <- c(xs, ys)
+str1 <- c('aaa', 'bbb')
+
+# unikac petli
+
+tcrossprod(ys,xs)
+
+# wektor jest struktura homogeniczna, w czasie utworzenia
+# nastepuje konwersja to typu wspolnego (str)
+v1 <- c(1, "abc")
+v1
+v1 <- c(v1, TRUE)
+v1
+v1 <- c(v1, c(1,2))
+v1
+
+q() # po nacisnieciu klawisza [ENTER] w oknie dialogowym wybieramy opcje "nie zapisuj"
+
+# -----------------------
+
+
+
+# -----------------------
+# -----------------------
+# -----------------------
+# -----------------------
+# -----------------------
+
