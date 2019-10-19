ef3938d0e47f85e659ef00af46b1dddee6bacbaa
ksatola <krzysztof@satola.net>
2019-10-19 19:50:01 +0200

    Update

--- session02/1_statystyka/intro2R-lab1.R
+++ session02/1_statystyka/intro2R-lab1.R
@@ -45,5 +45,37 @@ print("My name is Inigo Montoya.", "You killed my father.", "Prepare to die.") #
 cat(c('a','b'))
 cat('a', 'b')
 
+?getwd
+getwd()
+setwd("~/Documents/git/data-science-postgrad/session02/1_statistics")
 
+x1 <- 10
+.x2 <- 3.5
+x.3 <- "abc"
 
+ls()
+?ls
+ls(all.names = T)
+
+?rm
+rm(x.3)
+
+# -------------------- Clear Session
+
+x1 <- TRUE
+class(x1)
+?logical
+is.logical(x1)
+
+?numeric
+?integer
+?complex
+?character
+?vector
+
+0.1 + 0.1 == 0.2
+0.1 + 0.1 + 0.1 == 0.3
+0.1 + 0.1 + 0.1
+
+maxN <- 2^.Machine$double.digits
+maxN + 1 == maxN
