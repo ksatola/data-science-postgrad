24a82bcf1203c34de2b35df73d5adf46f93e9191
ksatola <krzysztof@satola.net>
2019-10-20 14:09:06 +0200

    Update

---
+++ session02/4_statystyka/Stat-2_RScript01.R
@@ -0,0 +1,31 @@
+
+# Generuj dane ----
+# Wygeneruj losowo zestaw danych
+# x - dziedzina
+?sample()
+
+set.seed(5) # reset generatoras
+xs <- sample(x=1:6, size=6, replace=TRUE, prob=NULL)
+
+print(xs)
+
+# 1. Obliczanie sredniej z proby ----
+print(mean(xs))
+
+
+# 2. Obliczanie mediany ----
+
+# ------------------------------
+# 
+
+# ------------------------------
+# 
+
+# ------------------------------
+# 
+
+# ------------------------------
+# 
+
+# ------------------------------
+# 
